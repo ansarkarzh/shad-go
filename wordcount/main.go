e1b3e802f42fa1ef18b23d8c3187cf711907d8fd
Ansar Karzhaspayev <ansarkarzh@10.215.168.197-vpn.dhcp.yndx.net>
2026-08-17 23:45:33 +0300

    solved wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,31 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	//"path/filepath"
+	s "strings"
+)
 
+func main() {
+	fileNames := os.Args[1:]
+	lines := make(map[string]int)
+	for _, name := range fileNames {
+		// path := filepath.Join(os.TempDir(), name)
+		dat, err := os.ReadFile(name)
+		if (err != nil) {
+			panic(err)
+		}
+
+		for _, line := range s.Split(string(dat), "\n") {
+			lines[line]++
+		}
+	}
+
+	for line, count := range lines {
+		if (count >= 2) {
+			fmt.Printf("%d\t%s\n", count, line)
+		}
+	}
 }
