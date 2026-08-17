e1599155ab56fa8aab3672c564edc9175fe7b41b
Ansar Karzhaspayev <ansarkarzh@10.215.168.197-vpn.dhcp.yndx.net>
2026-08-18 00:08:55 +0300

    solved urlfetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,24 @@
 
 package main
 
-func main() {
+import (
+	"os"
+	"fmt"
+	"net/http"
+	"io"
+)
 
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			os.Exit(1)
+		}
+		fmt.Printf("%s", body)
+	}
 }
