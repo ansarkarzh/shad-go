823435acaa9b039c751fea79fd508c0626259022
Ansar Karzhaspayev <ansarkarzh@10.215.168.197-vpn.dhcp.yndx.net>
2026-08-18 00:18:43 +0300

    better urlfetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -9,17 +9,24 @@ import (
 	"io"
 )
 
-func main() {
-	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
-		if err != nil {
-			os.Exit(1)
-		}
-		defer resp.Body.Close()
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			os.Exit(1)
-		}
-		fmt.Printf("%s", body)
+func fetch(url string, w io.Writer) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
 	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(w, resp.Body)
+	return err
+}
+
+func main() {
+	exitCode := 0
+	for _, url := range os.Args[1:] {
+		if err := fetch(url, os.Stdout); err != nil {
+			fmt.Printf("ERERE")
+			exitCode = 1
+		}
+	}
+	os.Exit(exitCode)
 }
