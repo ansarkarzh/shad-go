7429f1c1eedb27afd8338605548ffe89942afe01
Ansar Karzhaspayev <ansarkarzh@172.28.216.240-red.dhcp.yndx.net>
2026-09-02 10:55:19 +0300

    solve fetchall

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,38 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func fetch(url string, channel chan string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		channel <- "error"
+		return
+	}
+	defer resp.Body.Close()
+
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		channel <- "error"
+		return
+	}
+
+	channel <- "success"
+}
+
+func main() {
+	results := make(chan string)
+	args := os.Args[1:]
+	for _, url := range args {
+		go fetch(url, results)
+	}
+	for _ = range len(args) {
+		fmt.Println(<-results)
+	}
+	os.Exit(0)
 }
