3b35888f2028b0fe8f73748eb51aec2b96156c68
Ansar Karzhaspayev <ansarkarzh@MacBook-Pro.local>
2026-09-06 11:50:16 +0300

    solve business

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,11 @@
 
 package hotelbusiness
 
+import (
+	"cmp"
+	"slices"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -12,6 +17,39 @@ type Load struct {
 	GuestCount int
 }
 
+type move struct {
+	day     int
+	checkIn bool
+}
+
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	moves := make([]move, 0, len(guests)*2)
+	for _, guest := range guests {
+		moves = append(moves, move{guest.CheckInDate, true})
+		moves = append(moves, move{guest.CheckOutDate, false})
+	}
+	slices.SortFunc(moves, func(a, b move) int {
+		return cmp.Compare(a.day, b.day)
+	})
+
+	loads := []Load{}
+	curGuestCnt := 0
+	curday := 0
+	prevGuestCnt := 0
+	for i := 0; i < len(moves); {
+		curday = moves[i].day
+		for i < len(moves) && moves[i].day == curday {
+			if moves[i].checkIn {
+				curGuestCnt++
+			} else {
+				curGuestCnt--
+			}
+			i++
+		}
+		if curGuestCnt != prevGuestCnt {
+			loads = append(loads, Load{curday, curGuestCnt})
+			prevGuestCnt = curGuestCnt
+		}
+	}
+	return loads
 }
